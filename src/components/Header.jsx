e56190e540c3754e14b90f48cafecfae87922aa9
andre09999 <andre09999@live.com>
2023-05-20 13:19:58 -0300

    header feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
+import '../App.css';
+import logs from './66bfc5ec4227a89006ec7431f8f2fb20-removebg-preview.png'
+import { useState, useEffect } from 'react';
 
 function Header() {
+  const [account, setAccount] = useState( )
+  useEffect(()=> {
+    const countJson= localStorage.getItem('count')
+    setAccount(JSON.parse(countJson))
+  },[])
+  console.log()
     return (
       <header>
-     <h1>header</h1>
+     <img className="logo" src={logs}alt="logo football" />
+     <div>
+      <h3>User: {account?.firstname} {account?.lastname}</h3>
+      <h3>Email: {account?.email}</h3>
+     </div>
         </header>
     );
   }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,25 @@
 import { Route, Switch, Redirect, link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import api from '../service/Api';
 import '../App.css';
 
 function Home() {
+  const[paises, SetPaises] = useState()
+  const [code, setCode] = useState()
+  
+  useEffect(()=> {
+    setCode(localStorage.getItem('key'))
+    api.get("/countries",{
+      headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key':  code,
+      }
+    }).then((res)=> {
+      SetPaises(res.data.response)
+    }).catch((error) => {
+      console.log(error)
+    })
+  },[])
   return (
     <main>
         <h1>home</h1>
