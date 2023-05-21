7ffb5e63288fa8224f98d81a146d017ad5f289df
andre09999 <andre09999@live.com>
2023-05-20 22:30:01 -0300

    update css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
       <footer>
-     <h1>Desenvolvido por Andre Luis</h1>
+     <h1>Desenvolvido por <a href="https://www.linkedin.com/in/dev-andre-front-end/" target="_blank" rel="noreferrer">Andre Luis</a></h1>
         </footer>
     );
   }
