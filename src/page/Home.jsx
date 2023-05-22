2f4dab91ca0e897682576e28cb174f26734fd2be
andre09999 <andre09999@live.com>
2023-05-22 00:04:21 -0300

    arrumando bugs api e css e grafico

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,14 @@
 import { useHistory } from 'react-router-dom';
-import { BarChart, Bar,  XAxis,  YAxis,  CartesianGrid,  Tooltip,  Legend} from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
 import { useState, useEffect } from 'react';
 import api from '../service/Api';
 import '../App.css';
@@ -17,42 +26,98 @@ function Home() {
   const[escolhatime, Setescolhatime] = useState()
   const[minutos, Setminutos] = useState()
   const [code, setCode] = useState()
+
+  const[ grafico, setGrafico]= useState( [
+   {name: '0-15', gols: 4},
+    {name: '16-30', gols: 17},
+   {name: '31-45', gols: 11},
+    {name: '46-60', gols: 13},
+   {name: '61-75', gols: 10},
+{name: '76-90', gols: 8},
+    {name: '90-105', gols: 3},
+    {name: '106-120', gols: null}
+  ])
+
+  const aaa = [{"0-15": {
+    "total": 37,
+    "percentage": "6.06%"
+    },
+    "16-30": {
+    "total": 1,
+    "percentage": "25.76%"
+    },
+    "31-45": {
+    "total": 20,
+    "percentage": "16.67%"
+    },
+    "46-60": {
+    "total": 70,
+    "percentage": "19.70%"
+    },
+    "61-75": {
+    "total": 0,
+    "percentage": "15.15%"
+    },
+    "76-90": {
+    "total": 4,
+    "percentage": "12.12%"
+    },
+    "91-105": {
+    "total": 3,
+    "percentage": "4.55%"
+    },
+    "106-120": {
+    "total": null,
+    "percentage": null
+    }}]
   const history = useHistory();
+ 
+  const chave = async () => {
+    await setCode(localStorage.getItem('key'))
+       if(!localStorage.getItem('key')){
+         history.push('/')
+       }
+   }
 
 useEffect(()=> {
-  setCode(localStorage.getItem('key'))
-    if(!localStorage.getItem('key')){
-      history.push('/')
-    }
-  api.get("/https://v3.football.api-sports.io/leagues/seasons",{
-    headers: {
-      'x-rapidapi-host': 'v3.football.api-sports.io',
-      'x-rapidapi-key':  code,
-    }
-  }).then((res)=> {
-    SetAno(res.data)
-  }).catch((error) => {
-    console.error(error)
-  })
-},[])
-
-  useEffect(()=> {
-    if(ano?.length > 0){
-      api.get("/countries",{
+  chave()
+  const sano = async () =>{
+    await api.get("/leagues/seasons",{
         headers: {
           'x-rapidapi-host': 'v3.football.api-sports.io',
           'x-rapidapi-key':  code,
         }
       }).then((res)=> {
-        SetPaises(res.data)
+        SetAno(res.data)
       }).catch((error) => {
-        console.log(error.response)
+        console.error(error)
       })
     }
-  },[ano])
+    sano()
+},[code])
 
   useEffect(()=> {
-    if(paises?.length > 0){
+    const country = async  () => {
+      if(escolhaTemporada?.length > 0){
+        await api.get("/countries",{
+          headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key':  code,
+          }
+        }).then((res)=> {
+          SetPaises(res.data.response)   
+        }).catch((error) => {
+          console.log(error.response)
+        })
+      }
+    }
+    country()
+    
+  },[escolhaTemporada, code])
+
+  useEffect(()=> {
+
+    if(escolhapaises?.length > 0){
       api.get(`/leagues?country=${escolhapaises}&season=${escolhaTemporada}`,{
         headers: {
           'x-rapidapi-host': 'v3.football.api-sports.io',
@@ -65,11 +130,13 @@ useEffect(()=> {
         console.log(error.response)
       })
     }
-  },[paises])
+  },[escolhapaises])
 
   useEffect(()=> {
-    if(liga?.length > 0){
-      api.get(`/teams?league=${escolhaliga}&season=${escolhaTemporada}`,{
+    if(escolhaliga?.length > 0){
+      const all = liga?.response?.filter((a) =>  a.league.name === escolhaliga )
+      Setescolhaliga(all[0]?.league?.id)
+      api.get(`/teams?league=${all[0]?.league?.id}&season=${escolhaTemporada}`,{
         headers: {
           'x-rapidapi-host': 'v3.football.api-sports.io',
           'x-rapidapi-key':  code,
@@ -80,11 +147,13 @@ useEffect(()=> {
         console.log(error.response)
       })
     }
-  },[liga])
+  },[escolhaliga])
 
   useEffect(()=> {
-    if(time?.length > 0){
-      api.get(`/players?team=${time}&league=${escolhaliga}&season=${escolhaTemporada}`,{
+    if(escolhatime?.length > 0){
+      const all = time?.response?.filter((a) =>  a.team.name === escolhatime)
+      Setescolhatime(all[0]?.team?.id)
+      api.get(`/players?team=${all[0]?.team?.id}&league=${escolhaliga}&season=${escolhaTemporada}`,{
         headers: {
           'x-rapidapi-host': 'v3.football.api-sports.io',
           'x-rapidapi-key':  code,
@@ -95,7 +164,7 @@ useEffect(()=> {
         console.log(error)
       })
     }
-  },[time])
+  },[escolhatime])
   useEffect(()=>{
     if(formação?.length > 0){
 
@@ -107,12 +176,26 @@ useEffect(()=> {
       }).then((res)=> {
         setestatiticas(res.data)
         console.log(res.data.response.gols)
-        Setminutos(res.data.response.gols)
+        Setminutos(res.data.response.gols.for.minute)
+        let golos = Object.assign({}, Object.values(res.data.response.gols.for.minute[0]).map((a,i)=>a.total))
+
+        Object.keys(golos).forEach((a)=> {
+        
+          let newKwy = Object.keys(res.data.response.gols.for.minute[0])[a]
+          setGrafico()
+          const ab = { name : golos[newKwy], gols: golos[a] }
+          setGrafico(grafico,ab)
+          delete golos[a]
+        })
+        console.log(grafico)
+        
       }).catch((error) => {
         console.log(error)
       })
     }
   },[formação])
+  
+  
 
   return (
     <main>
@@ -120,26 +203,26 @@ useEffect(()=> {
         <select name="ano" id="" onChange={({target})=> SetescolhaTemporada(target.value)} >
         {
           ano?.response?.map((i)=> (
-            <option key={i.code}>{i.name}</option>
+            <option key={i}>{i}</option>
           ))
         }
         </select>
         <select name="pais" id="" onChange={({target})=> Setescolhapaises(target.value)}>
         {
-          paises?.response?.map((i,a)=> (
-            <option key={i.code}>{i}</option>
+          paises?.map((i,a)=> (
+            <option key={i.name}>{i.name}</option>
           ))
         }</select>
          <select name="liga" id="" onChange={({target})=> Setescolhaliga(target.value)}>
         {
           liga?.response?.map((i,a)=> (
-            <option key={i.code}>{i}</option>
+           <option key={i.league.id}>{i.league.name}</option>
           ))
         }</select>
         <select name="time" id="" onChange={({target})=> Setescolhatime(target.value)}>
         {
           time?.response?.map((i,a)=> (
-            <option key={i.code}>{i}</option>
+            <option key={i.team.id}>{i.team.name}</option>
           ))
         }
          </select>
@@ -160,17 +243,11 @@ useEffect(()=> {
               <p>ganhou : {a.fixtures.wins.total}</p>
               <p>empatou: {a.fixtures.draws.total}</p>
               <p>perdeu: {a.fixtures.loses.total}</p>
-              <BarChart width={600} height={300} data={minutos}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey={minutos} />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="gols" fill="#8884d8" />
-              </BarChart>
+              
             </div>
           ))
         }
+        
         <table>
           <thead>
             <tr id='cabecalho'>
@@ -198,7 +275,48 @@ useEffect(()=> {
           </tbody>
         </table>
         </div>
-      
+        <div className='container-grafico-celular'>
+       <LineChart 
+        width={300}
+        height={150}
+        data={grafico}
+        className='grafico'
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+        <Line
+          type='linear'
+          dataKey='gols'
+          stroke='black'
+          activeDot={{ stroke: 'red', strokeWidth: 2, r: 10 }}
+          />
+        <CartesianGrid strokeDasharray='3 3'/>
+        <Tooltip/>
+        <YAxis tick={{stroke: 'brown', strokeWidth: 1}}/>
+        <XAxis dataKey='name' tick={{stroke: 'brown', strokeWidth: 1}}/>
+        <Legend />
+      </LineChart>
+      </div>
+      <div className='container-grafico-computador'>
+       <LineChart 
+        width={600}
+        height={300}
+        data={grafico}
+        className='grafico'
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+        <Line
+          type='linear'
+          dataKey='gols'
+          stroke='black'
+          activeDot={{ stroke: 'red', strokeWidth: 2, r: 10 }}
+          />
+        <CartesianGrid strokeDasharray='3 3'/>
+        <Tooltip/>
+        <YAxis tick={{stroke: 'brown', strokeWidth: 1}}/>
+        <XAxis dataKey='name' tick={{stroke: 'brown', strokeWidth: 1}}/>
+        <Legend />
+      </LineChart>
+      </div>
       </main>
   );
 }
